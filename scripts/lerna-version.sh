a430d6122db3f96dfbe708d8205cfec1488b825d
Claudio De Angelis <claudio.deangelis@beemit.com.au>
2022-07-27 10:03:18 +1000

    feat(test): some

---
+++ scripts/lerna-version.sh
@@ -0,0 +1,24 @@
+# npm config set user 0 # https://timjrobinson.com/fixing-node-gyp-permission-denied-when-running-as-root/
+# npm config set unsafe-perm true
+
+# git fetch --prune --tags
+# git remote -v
+
+# source build/bash/create-child-triggers.sh
+
+# if [[ "${ENVIRONMENT}" != "prod" ]]; then
+#     branch="${CI_COMMIT_REF_NAME}"
+#     create_triggers generated-triggers.yml
+#     # normalize id names from branch names: feat/BAAS-123 -> feat-baas-123
+#     preid=$(git rev-parse --abbrev-ref HEAD | sed 's/\//-/g' | sed 's/[^0-9a-zA-Z\-]//g' | tr '[:upper:]' '[:lower:]')
+#     # publish packages in prerelease for feature branches, develop, stage, release
+#     npx lerna publish --conventional-prerelease --preid $preid --pre-dist-tag $CI_COMMIT_BRANCH --yes
+# else
+#     # npx lerna version --conventional-graduate --yes
+#     # publish stable versions for prod
+#     npx lerna publish --conventional-graduate --yes
+# fi
+
+preid=$(git rev-parse --abbrev-ref HEAD | sed 's/\//-/g' | sed 's/[^0-9a-zA-Z\-]//g' | tr '[:upper:]' '[:lower:]')
+# publish packages in prerelease for feature branches, develop, stage, release
+npx lerna version --conventional-prerelease --preid $preid
